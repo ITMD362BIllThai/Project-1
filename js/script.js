16fc2c9ff818b7ca7f455d52ca119e3d0e3e0ecd
BillThai <bthai@hawk.iit.edu>
2019-10-29 06:16:14 -0500

    Got alert to work with html

--- js/script.js
+++ js/script.js
@@ -1,41 +1,12 @@
 function init()
 {
-  var fname = document.getElementById("").value;
-  var lname = document.forms["form_body"]["lastname"].value;
-  var email = document.forms["form_body"]["email"].value;
-  var phone = document.forms["form_body"]["phonefield"].value;
+  var fname = document.getElementById("first_name").value;
+  var lname = document.getElementById("last_name").value;
+  var email = document.getElementById("Email").value;
+  var phone = document.getElementById("Phone_Number").value;
   
-  if (fname.value == "")
-  {
-    alert("Please enter your first name.");
-	fname.focus();
-	return false;
-  }
-  
-  if (lname.value == "")
-  {
-    alert("Please enter your last name.");
-	lname.focus();
-	return false;
-  }
-  
-  if (email.value == "")
-  {
-    alert("Please enter your email address.");
-	email.focus();
-	return false;
-  }
-  
-  if (phone.value == "")
-  {
-    alert("Please enter your phone number.");
-	phone.focus();
-	return false;
-  }
-  
-  else
-  {
-    alert("There is only the Emperor, and he is our Shield and Protector.");
-	return false;
-  }
+
+    alert("There is only the Emperor, "+email+"and he is our Shield and Protector.");
+
 }
+
